321e67f9c53e31d0f94137dadd04710f09ed3b14
NeoBitose <ramadhanalif430@gmail.com>
2024-06-29 02:09:59 +0700

    feat(category): add read category

--- config/database.go
+++ config/database.go
@@ -10,7 +10,7 @@ import (
 var DB *sql.DB
 
 func ConnectDB() {
-	db, err := sql.Open("mysql", "root:@/db_golang_crud")
+	db, err := sql.Open("mysql", "root:@/db_golang_crud?parseTime=true")
 	if err != nil {
 		panic(err)
 	}
---
+++ models/categorymodel/categorymodel.go
@@ -0,0 +1,29 @@
+package categorymodel
+
+import (
+	"crud_go_native/config"
+	"crud_go_native/entities"
+)
+
+func GetAll() []entities.Category { 
+	rows, err := config.DB.Query(`SELECT * FROM categories`) 
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	var categories []entities.Category
+
+	for rows.Next() {
+		var category entities.Category
+		err := rows.Scan(&category.Id, &category.Name, &category.CreatedAt, &category.UpdatedAt)
+		if err != nil {
+			panic(err)
+		}
+
+		categories = append(categories, category)
+	}
+
+	return categories
+}
